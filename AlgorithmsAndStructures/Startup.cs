fcc28c36026dda0a81a4b5cce2fa6f6b4e6bd311
v.samutin <v.samutin@itwebnet.ru>
2020-12-21 22:52:57 +0300

    add React, startup customization, add Index

--- AlgorithmsAndStructures/Startup.cs
+++ AlgorithmsAndStructures/Startup.cs
@@ -1,12 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using React.AspNet;
 
 namespace AlgorithmsAndStructures
 {
@@ -16,24 +19,33 @@ namespace AlgorithmsAndStructures
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+            services.AddControllers();
+            services.AddMvc();
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName).AddChakraCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+            app.UseReact(config => { });
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}");
             });
         }
     }
---
+++ AlgorithmsAndStructures/wwwroot/js/app.jsx
@@ -0,0 +1,9 @@
+﻿class MainComponent extends React.Component {
+    render() {
+        return <h1>Start</h1>;
+    }
+}
+ReactDOM.render(
+    <MainComponent />,
+    document.getElementById("content")
+);
